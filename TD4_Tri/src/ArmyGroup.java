e8c8c382751fcaefe9699d795a56adcbcde58282
Tri dep <nguyentri0401@gmail.com>
2018-05-27 08:27:36 +0700

    remove metadata folder

--- TD4_Tri/src/ArmyGroup.java
+++ TD4_Tri/src/ArmyGroup.java
@@ -1,4 +1,11 @@
 import java.util.ArrayList;
 public class ArmyGroup {
-
+	private ArrayList<Soldier> subgroup = new ArrayList<Soldier>();
+	private ArrayList<ArrayList<Soldier>> group = new ArrayList<subgroup>();
+	private String name;
+	
+	
+	public ArmyGroup(Soldier soldier) {
+		group.add(e)
+	}
 }
