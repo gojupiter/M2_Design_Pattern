1be1c687dd2fe4de773c646685d20daba6f1ac64
GO <nguyentri0401@gmail.com>
2018-06-22 19:36:31 +0700

    project

---
+++ Pacman/game/Universe.java
@@ -0,0 +1,12 @@
+package game;
+
+public interface Universe {
+
+	public abstract void addGameEntity(GameEntity gameEntity);
+
+	public abstract void removeGameEntity(GameEntity gameEntity);
+
+	public abstract void animate();
+
+	public abstract void collision();
+}
